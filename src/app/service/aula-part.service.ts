c4337aad1fd05b9c992ec9d1425cce8c5d29a9e7
beboantonio <beboantonio1@hotmail.com>
2021-09-29 19:28:22 -0300

    create a list with some users for test

--- src/app/service/aula-part.service.ts
+++ src/app/service/aula-part.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Perfil } from 'src/enum/Perfil';
+import { Aula } from 'src/models/Aula';
+import { Usuario } from 'src/models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AulaPartService {
+  usuarios: Usuario[];
+  aulas: Aula[];
+  logado: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuarios = [
+      {id: 1,nome: 'fulano de tal', perfil: Perfil.professor , login: 'fulanodetal@teste.com', senha: '123' },
+      {id: 2,nome: 'beltrano', perfil: Perfil.aluno , login: 'beltrano@teste.com', senha: '123' }
+    ];
+
+  }
 }
--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,6 +1,9 @@
+import { Perfil } from 'src/enum/Perfil';
+
 export interface Usuario {
     id: number;
     nome: string;
     login: string;
     senha: string;
+    perfil: Perfil;
 }
